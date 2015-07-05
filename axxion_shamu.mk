66f027ae33dadadaed15498e462e7feb6f23cafe
Sira Cuervo <fl.siracusa@outlook.com.ar>
2015-07-05 20:12:02 -0300

    Axxionize 2

--- axxion_shamu.mk
+++ axxion_shamu.mk
@@ -18,18 +18,18 @@
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Inherit APNs list
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/axxion/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/axxion/config/common.mk)
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/moto/shamu/device.mk)
 
-PRODUCT_NAME := omni_shamu
+PRODUCT_NAME := axxion_shamu
 PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
